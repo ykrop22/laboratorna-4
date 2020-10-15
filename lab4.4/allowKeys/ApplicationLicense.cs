3010407cb42a295ae3b99a3a82ee07e09b23a0c4
ykrop22 <45872447+ykrop22@users.noreply.github.com>
2020-10-15 23:06:58 +0300

    Add files via upload

---
+++ lab4.4/allowKeys/ApplicationLicense.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace allowKeys
+{
+    class ApplicationLicense
+    {
+        private const string trialKey = "qwe123";
+        private const string commonKey = "q12qwe";
+        private const string proKey = "reqw12344";
+        public string typeVersion { get; private set; }
+        public ApplicationLicense(string key)
+        {
+            typeVersion = "free";
+            switch (key)
+            {
+                case trialKey:
+                    AllowTrial();
+                    break;
+                case commonKey:
+                    AllowCommon();
+                    break;
+                case proKey:
+                    AllowPro();
+                    break;
+                    
+            }
+
+        }
+        public void AllowTrial()
+        {
+            Debug.WriteLine("TrialVersion");
+            typeVersion = "trial";
+        }
+        public void AllowCommon()
+        {
+            Debug.WriteLine("CommonVersion");
+            typeVersion = "common";
+
+        }
+        public void AllowPro()
+        {
+            Debug.WriteLine("ProVersion");
+            typeVersion = "pro";
+        }
+    }
+}
---
+++ lab4.4/allowKeys/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace allowKeys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter license key: ");
+            string key = Console.ReadLine();
+            ApplicationLicense myApp = new ApplicationLicense(key);
+            Console.WriteLine("Start Work?: (press any key)");
+            Console.ReadKey();
+            if(myApp.typeVersion != "free")
+            {
+                while (true)
+                {
+                    Console.WriteLine($"{myApp.typeVersion} version working");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You are using the free version. Upgrade your license or try a trial period");
+            }
+
+        }
+    }
+}
